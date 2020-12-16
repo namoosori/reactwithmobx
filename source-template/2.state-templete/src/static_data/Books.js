c96f399407b72dde141ad68b0fc8181f1a318b9c
kwonki-Jin <kkjin@nextree.io>
2020-12-16 13:21:47 +0900

    add state-exam files

---
+++ source-template/2.state-templete/src/static_data/Books.js
@@ -0,0 +1,61 @@
+const Books = [
+    {
+        ISBN: '9781617293986',
+        title: 'Spring Microservices in Action',
+        author: 'John Carnell',
+        publisher: 'Manning',
+        price: 59.92,
+        imgUrl: 'book_images/spring.jpg',
+        introduce: 'Spring Boot and Spring Cloud offer Java developers an easy ' +
+                    'migration path from traditional monolithic Spring applications to ' +
+                    'microservice-based applications that can be deployed to multiple ' +
+                    'cloud platforms.'
+    },
+
+    {
+        ISBN: '9781491954621',
+        title: 'Learning React',
+        author: 'Alex Banks , Eve Porcello',
+        publisher: 'O`Relly',
+        price: 49.19,
+        imgUrl: 'book_images/react.jpg',
+        introduce: 'Interested in React but find yourself confused ' + 
+                    'about how to use it with the Flux architecture? This is your book.'
+    },
+
+    {
+        ISBN: '9780321125217',
+        title: 'Domain-Driven Design',
+        author: 'Eric Evans',
+        publisher: 'Addison-Wesley',
+        price: 73.33,
+        imgUrl: 'book_images/domain.jpg',
+        introduce: 'Explains how to incorporate effective domain modeling into the ' +
+                    'software development process.'
+    },
+
+    {
+        ISBN: '9781617294471',
+        title: 'Kafka Streams in Action',
+        author: 'Bill Bejeck',
+        publisher: 'Manning',
+        price: 43.82,
+        imgUrl: 'book_images/kafka.jpg',
+        introduce: 'SKafka Streams is a library designed to allow for easy ' + 
+                    'stream processing of data flowing into a Kafka cluster.'
+    },
+
+    {
+        ISBN: '9780596805524',
+        title: 'JavaScript',
+        author: 'John Carnell',
+        publisher: 'O`Relly',
+        price: 49.19,
+        imgUrl: 'book_images/javascript.jpg',
+        introduce: 'Whether you need an example-driven programmer\'s guide ' + 
+                    'or a complete desk reference, JavaScript: The Definitive Guide is ' + 
+                    'the most authoritative book on this key language for Web 2.0 applications.'
+    }
+]
+
+export default Books;
